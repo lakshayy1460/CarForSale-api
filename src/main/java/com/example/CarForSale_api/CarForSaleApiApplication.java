f8e526d23efb36167a5424936b0c2d5187c34a9b
Lakshay Sharma <lakshay.sharma8113@gmail.com>
2025-09-24 10:49:44 +0530

    attaching files

---
+++ src/main/java/com/example/CarForSale_api/CarForSaleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarForSale_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarForSaleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarForSaleApiApplication.class, args);
+	}
+
+}
